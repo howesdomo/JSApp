31424736ad0ff884592240abb10d7bb4d3ed02fb
HowesDOMO <164572429@qq.com>
2023-07-25 16:03:06 +0800

    加入emoji放大器

---
+++ 20230725_ZoomInEmoji/script/queue.js
@@ -0,0 +1,27 @@
+class Queue {
+	constructor(size) {
+		if (Number.isInteger(size) == false) {
+			throw `参数size不是 Number 类型。size: ${size}`;
+		}
+
+		if (size <= 0) {
+			throw `参数size请传入正整数。size: ${size}`;
+		}
+
+		this.items = [];
+		this.maxSize = size;
+	}
+
+	enqueue(item) {
+		if (this.items.length >= this.maxSize) {
+			this.items.shift();
+		}
+        this.items.push(item);
+        return this.items;
+	}
+
+	dequeue() {
+        this.items.shift();
+        return this.items;
+	}
+}
