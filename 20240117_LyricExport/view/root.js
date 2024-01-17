11d0995bc0869c6c7c1f90971706ecbefacb86a6
HowesDOMO <164572429@qq.com>
2024-01-17 17:54:18 +0800

    优化歌词导出 1. 使用 Promise 对 mp3 文件进行逐个分析 2. 优化 UI 体验

--- 20240117_LyricExport/view/root.js
+++ 20240117_LyricExport/view/root.js
@@ -10,7 +10,11 @@ const view_root =
     <el-divider content-position="left">{{mListInfo}}</el-divider>
 
     <div>
-        <el-table :data="mList" style="width: 100%">
+        <el-table :data="mList" 
+            style="width: 100%" 
+            v-loading="mIsBusy"
+            :element-loading-text="mBusyContent"
+        >
             <el-table-column prop="FileName" label="FileName">
             </el-table-column>
             <el-table-column width="120" label="是否成功">
@@ -27,7 +31,9 @@ const view_root =
     {
         return {
             mList: [],
-            mListInfo: ""
+            mListInfo: "",
+            mIsBusy: false,
+            mBusyContent: ""
         };
     },
     mounted: function() {
@@ -35,73 +41,94 @@ const view_root =
     },
     methods: {
         btnSelectMP3: function() {
+            this.mIsBusy = true;
             this.$refs.upload0.click();
         },
 
-        upload0_ChangeEvent_Handler: function (event) {
+        upload0_ChangeEvent_Handler: async function (event) {
             this.mList = []; // 清空
+            let tempList = [];
+
+            console.log(`进入 upload0_ChangeEvent_Handler`);
+            console.log(`用户选择共 ${event.target.files.length} 个文件`);
 
             const files = event.target.files;
             const that = this;
 
-            let list = [];
-            //#region 分析
+            //#region 逐个mp3文件分析里面的歌词信息
+
             for (let index = 0; index < files.length; index++) {
-                const fileReader = new FileReader();
+                // const fileReader = new FileReader(); // 无需fileReader
 
                 let selectedFile = files[index];
-                this.fileName = selectedFile.name;
-                fileReader.readAsArrayBuffer(selectedFile);
 
-                jsmediatags.read(selectedFile, {
-                    onSuccess: function (tag) {
-                        if(tag.tags.lyrics && tag.tags.lyrics.lyrics) {
-                            const lyrics = tag.tags.lyrics.lyrics; // 歌词信息
+                console.log(`正在分析第 ${(index + 1)} ${selectedFile.name}`);
+                this.mBusyContent = `正在分析第 ${(index + 1)} ${selectedFile.name}`;
+                
+                // fileReader.readAsArrayBuffer(selectedFile); // 无需fileReader
 
-                            let toAdd = {
-                                "FileName": selectedFile.name,
-                                "IsSuccess": true,
-                                "ExceptionInfo": null,
-                                "LyricsFileName": `${selectedFile.name.substring(0, selectedFile.name.lastIndexOf('.'))}.lrc`,
-                                "Lyrics": lyrics
-                            };                                
-                            list.push(toAdd); // that.mList.push(toAdd);
-                        }
-                        else {
-                            let toAdd = {
-                                "FileName": selectedFile.name,
-                                "IsSuccess": false,
-                                "ExceptionInfo": `没有歌词信息. tag.tags.lyrics is undefined`
-                            };
-                            list.push(toAdd); // that.mList.push(toAdd);
-                        }
-                    },
-                    onError: function (error) {
-                        let toAdd = {
-                            "FileName": selectedFile.name,
-                            "IsSuccess": false,
-                            "ExceptionInfo": `${error.type}: ${error.info}`
-                        };
-                        list.push(toAdd); // that.mList.push(toAdd);
-                    }
-                });
+                try 
+                {
+                    let tag = await that.execute_read_mp3_lyrics_Stepbystep(selectedFile); // 无需fileReader, 只需将 selectedFile ( 即系 files[index] ), 传比 jsmediatags.read(selectedFile, ... ) 
 
-                if(index + 1 == files.length) {
-                    console.log(`读取完毕 - ${Date.now().toString()}`);
-                    setTimeout(()=> {
-                        this.mList = Enumerable.from(list).orderBy(i=>i.IsSuccess).toArray(); // 排序
+                    const lyrics = tag.tags.lyrics.lyrics; // 歌词信息
 
-                        const k0 = Enumerable.from(this.mList).where(i=>i.IsSuccess).count(i => 1);
-                        const k1 = Enumerable.from(this.mList).where(i=>!i.IsSuccess).count(i => 1);
-                        this.mListInfo = `共 ${this.mList.length} 个, 成功 ${k0} 个, 失败 ${k1} 个`;                            
-                    }, 2000);
+                    let toAdd = {
+                        "FileName": selectedFile.name,
+                        "IsSuccess": true,
+                        "ExceptionInfo": null,
+                        "LyricsFileName": `${selectedFile.name.substring(0, selectedFile.name.lastIndexOf('.'))}.lrc`,
+                        "Lyrics": lyrics
+                    };                                
+                    tempList.push(toAdd);
+                    console.log(`成功分析第 ${(index + 1)} ${selectedFile.name}`);
                 }
+                catch(error)
+                {
+                    let toAdd = {
+                        "FileName": selectedFile.name,
+                        "IsSuccess": false,
+                        "ExceptionInfo": `${error.type}: ${error.info}`
+                    };
+                    tempList.push(toAdd);
+                    console.log(`失败分析第 ${(index + 1)} ${selectedFile.name}`);   
+                }           
             }
-            //#endregion
+
+            //#endregion            
+
+            this.mList = Enumerable.from(tempList).orderBy(i=>i.IsSuccess).toArray(); // 排序
+
+            const k0 = Enumerable.from(this.mList).where(i=>i.IsSuccess).count(i => 1);
+            const k1 = Enumerable.from(this.mList).where(i=>!i.IsSuccess).count(i => 1);
+
+            this.mListInfo = `共 ${this.mList.length} 个, 成功 ${k0} 个, 失败 ${k1} 个`;
 
             // !!!坑点!!! 如果连续两次导入的文件路径一致, 则不会触发 change 事件
             event.target.value = null;
+
+            this.mIsBusy = false;
         },
+
+        execute_read_mp3_lyrics_Stepbystep: function (selectedFile) {
+            // 使用 await Promise 实现逐个 mp3 文件进行分析
+            return new Promise(function(resolve, reject) {
+                jsmediatags.read(selectedFile, {
+                    onSuccess: function(tag) {
+                        if(tag.tags.lyrics && tag.tags.lyrics.lyrics) {
+                            resolve(tag);   
+                        }
+                        else {
+                            reject(`没有歌词信息. tag.tags.lyrics is undefined`);
+                        }   
+                    },
+                    onError: function(error) {
+                        reject(error);
+                    }
+                });
+            });
+        },
+
         btnDownloadZip_OnClick: function () {
             if (this.mList == null || this.mList.length == 0) {
                 console.error('未选择音乐文件');
